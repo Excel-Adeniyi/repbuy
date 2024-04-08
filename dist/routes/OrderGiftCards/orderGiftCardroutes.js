f704562d0a14f08d6b075418244607903b42588b
Adeniyi Ezekiel <59603512+Excel-Adeniyi@users.noreply.github.com>
2024-04-08 13:28:15 +0100

    Delete dist directory

--- dist/routes/OrderGiftCards/orderGiftCardroutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const getGiftCard_1 = __importDefault(require("../../controller/OrderGiftCards/getGiftCard"));
-const orderGiftCards = new getGiftCard_1.default();
-exports.default = orderGiftCards;
