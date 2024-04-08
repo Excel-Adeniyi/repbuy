293eeb3a6b294f2e6a7db4894539fefe7d558230
Adeniyi Ezekiel <59603512+Excel-Adeniyi@users.noreply.github.com>
2024-04-08 13:28:15 +0100

    Delete dist directory

--- dist/helper/objectChecker.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isEmpty(obj) {
-    console.log("ERE", obj);
-    return Object.values(obj).length > 0;
-}
-exports.default = isEmpty;
