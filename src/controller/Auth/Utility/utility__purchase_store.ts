69c99b95792cbe4f2d78509945e019028e8c24e2
excel-adeniyi <adeniyiexcel@gmail.com>
2024-04-29 18:38:23 +0100

    completion of electric and cable screen

--- src/controller/Auth/Utility/utility__purchase_store.ts
+++ src/controller/Auth/Utility/utility__purchase_store.ts
@@ -16,10 +16,14 @@ class UtilityPurchaseController{
                 const response: any = await this.model.utilityPurchaseModel(data)
                 if(response.affectedRows === 1){
                     console.log("Success")
+                    res.status(200).json({Success: true, message: "Saved"})
+                }else{
+                    res.status(503).json({Success: false, message: "Unsaved"})
                 }
             }
         } catch (error) {
             console.log(error)
+            res.status(500). json({Success: false, message: "Internal Server Error"})
         }
     }
 }
