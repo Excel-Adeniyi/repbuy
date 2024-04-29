7e67528825447cc48c77e19df2001a17d911ac49
excel-adeniyi <adeniyiexcel@gmail.com>
2024-04-29 18:38:23 +0100

    completion of electric and cable screen

--- src/model/UtilityModel/utility_purchase_model.ts
+++ src/model/UtilityModel/utility_purchase_model.ts
@@ -2,29 +2,31 @@ import { QueryError, RowDataPacket } from "mysql2";
 import { Pool } from "mysql2/typings/mysql/lib/Pool";
 
 
-class UtilityPurchase{
-    private pool:Pool
+class UtilityPurchase {
+    private pool: Pool
 
 
-    constructor(pool: Pool){
+    constructor(pool: Pool) {
         this.pool = pool
     }
 
-    async utilityPurchaseModel(data: any): Promise<RowDataPacket[]>{
+    async utilityPurchaseModel(data: any): Promise<RowDataPacket[]> {
 
-        const query = "INSERT INTO current_purchase (operatorId, amount, countryCode, number,purchase_type, status) VALUES (?,?,?,?,?,?)"
+        const query = "INSERT INTO current_purchase (userId, number, amount, countryCode, operatorId, purchase_type, status, time) VALUES (?,?,?,?,?,?, 'pending', CURTIME())"
+        const params = [ ...Object.values(data) ]
+
         return new Promise((resolve, reject) => {
             this.pool.query(
-                query, (error: QueryError | null, results: RowDataPacket[]) =>{
-                    if(error){
+                query, params, (error: QueryError | null, results: RowDataPacket[]) => {
+                    if (error) {
                         console.log(error)
                         reject(error)
-                    }else{
+                    } else {
                         resolve(results)
                     }
                 }
             )
-        }) 
+        })
     }
 }
 
