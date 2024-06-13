a76e4e725cf848af18bb3c5c14793725d5a102a3
excel-adeniyi <adeniyiexcel@gmail.com>
2024-06-13 15:17:58 +0100

    created a save before submit for giftcard

--- src/controller/http/OrderGiftCards/giftcard_purchase_first_save.ts
+++ src/controller/http/OrderGiftCards/giftcard_purchase_first_save.ts
@@ -1,7 +1,47 @@
+import { Request, Response } from "express";
+import GiftCardSaveBeforePaymentModel from "../../../model/GiftCardModel/giftcard_save_before_payment";
 
 
 
 
-class GiftCardPurchase{
-    
+class GiftCardPurchaseBeforePayment {
+    private model: GiftCardSaveBeforePaymentModel
+
+
+    constructor(model: GiftCardSaveBeforePaymentModel) {
+        this.model = model
+    }
+
+    async cardPurchase(req: Request, res: Response): Promise<void> {
+        const payload = req.body
+        console.log(payload)
+        try {
+            const data1 = {
+                operatorId: payload.productId, amount: payload.amount, countryCode: payload.recipientPhoneDetails.countryCode, number: payload.recipientPhoneDetails.phoneNumber, userId: payload.userId, ntransactionId: payload.customIdentifier, purchase_type: payload.product_type
+            }
+            const data2 = {
+                userId: payload.userId, transaction_id_customer_id: payload.customIdentifier, recipient_email: payload.recipientEmail, recipient_phone: payload.recipientPhoneDetails.phoneNumber, country_code: payload.recipientPhoneDetails.countryCode, quantity: payload.quantity, unit_price: payload.unitPrice
+            }
+
+            const data3 = {
+                ntransactionId: payload.customIdentifier
+            }
+            const payloadData = await this.model.firstSave(data1, data2, data3)
+
+            if (payloadData.length > 0) {
+                res.status(200).json({ Success: true, message: payloadData })
+            } else {
+                res.status(503).json({ Success: false, message: "Currently not available" })
+            }
+
+        } catch (error: any) {
+            console.log(error)
+            res.status(500).json({ Success: false, message: "Internal Server Error" })
+
+
+
+        }
+    }
 }
+
+export default GiftCardPurchaseBeforePayment
--- src/routes/httpRoute/routes.ts
+++ src/routes/httpRoute/routes.ts
@@ -41,106 +41,90 @@ import creatededicatedAccount from "./CreateDedicatedAccountroutes/create_dedica
 import fcmStorageController from "./FCMStorageroutes/FCMStorageroutes";
 import singleDataController from "./SingleData/get_single_data";
 import translistcontroller from "./TransactionList/transaction_list_routes";
+import giftcardpurchasebeforePayment from "./OrderGiftCards/giftcard_purchase_first_save_to_db";
 
 
 let router = Router();
 
 router.post("/authtopup", GetAuthTopUp);
-router.post('/authGiftCard', GetGiftCardAuth)
 router.post('/authUtility', GetUntilityAuth)
-
 //verify airtime OTP and Complete Payment
 router.post('/otp/verify', (req, res) => airtimeController.VerifyOTP(req, res))
 router.post("/airtime/request", (req, res) => airtimeController.GetAirtime(req, res));
-
-
 //Data service
 router.post('/data/request', (req, res) => datarequestController.GetData(req, res))
+router.get("/balance", GetBalance);
+router.post('/ctoken', CsrfData)
+router.post('/operator', (req, res) => operatorsclass.GetOperators(req, res))
+router.get("/ios", JToken, (req, res) => {
+  iosController.getAllRecords(req, res);
+});
+router.post("/history", (req, res) => historyContoller.CreatePurchaseController(req, res))
+router.get("/getios", (req, res) => {
+  getSavedIosroute.getAllDbIosRecords(req, res);
+});
+//Paystack fundings
+router.post("/create/dedicated/account/paystack", (req, res) => {
+  creatededicatedAccount.CreateDedicatedAccount(req, res)
+})
+// #################### Login and create account ##############
+router.post('/create', (req, res) => signUPController.createAccount(req, res));
+router.post('/login', (req, res) => loginController.loginAccount(req, res))
+router.post('/googleSignUp', (req, res) => googleSignInControlller.createAccount(req, res))
+router.post('/userpin', (req, res) => otpcontroller.pincode(req, res))
+router.post('/userpin/verify', (req, res) => pincodeverifier.initiateVerify(req, res))
+router.post('/avater', (req, res) => signUPController.updateAvater(req, res))
 
 
-//Utility service
-router.post('/utility/payment', (req, res) => utilityPayment.ubiller(req, res))
-//Utility verify
-router.get('/utility/verify/:pid', (req, res) => utilityVerify.verify(req, res))
-
-//Save Utility Purchase in the db
-router.post('/utility/purchase/data', (req, res) => utilityPurchase.utilitypurchasecontroller(req, res))
-
-
-
+//##################### GIFT CARD #################
+router.post('/authGiftCard', GetGiftCardAuth)
 //get card by country
 router.get('/giftcard/getcards/:isoName', (req, res) => getCardsController.cardsReq(req, res))
 //get card by id
 router.get('/giftcard/getcard/:id', (req, res) => getGiftcardById.cardsReq(req, res))
 //giftcard Payment Purchase through Paystack
 router.post('/giftcard/order', (req, res) => giftcardProcessOrder.processPayment(req, res))
+router.post("/orderGiftcard", JToken, (req, res) => {
+  orderGiftCards.OrderGiftcardsProducts(req, res);
+});
+router.get("/getGiftCardProducts", JToken, (req, res) => {
+  getallgiftcardProducts.GetAllGiftCardProducts(req, res)
+})
+router.post('/giftcard/save/bpayment', (req,res) => giftcardpurchasebeforePayment.cardPurchase(req, res))
 
+
+
+//################ UTILITY ##############\
+//Utility service
+router.post('/utility/payment', (req, res) => utilityPayment.ubiller(req, res))
+//Utility verify
+router.get('/utility/verify/:pid', (req, res) => utilityVerify.verify(req, res))
+//Save Utility Purchase in the db
+router.post('/utility/purchase/data', (req, res) => utilityPurchase.utilitypurchasecontroller(req, res))
 router.get('/utilitybiller', UtilityBiller)
 
 
-router.post("/otp", (req, res) => otpcontroller.CreateHISTORYController(req, res))
-router.post('/otp/request', (req, res) => otpcontroller.recieveOTP(req, res))
-
-router.post('/userpin', (req, res) => otpcontroller.pincode(req, res))
-router.post('/userpin/verify', (req, res) => pincodeverifier.initiateVerify(req, res))
-
-
-router.post('/avater', (req, res) => signUPController.updateAvater(req, res))
-
-router.get("/balance", GetBalance);
-router.post('/ctoken', CsrfData)
-
-//Login and create account
-
-router.post('/create', (req, res) => signUPController.createAccount(req, res));
-router.post('/login', (req, res) => loginController.loginAccount(req, res))
-router.post('/googleSignUp', (req, res) => googleSignInControlller.createAccount(req, res))
-
-router.post('/operator', (req, res) => operatorsclass.GetOperators(req, res))
-router.get("/ios", JToken, (req, res) => {
-  iosController.getAllRecords(req, res);
-});
-
-//Payment gateway:
+//############# Payment gateway: ###########
 router.post('/getreference', (req, res) => paymentinit.initializePayment(req, res))
 router.post('/verifyPayment', (req, res) => verifypay.verifyPayment(req, res))
 
 
-router.post("/history", (req, res) => historyContoller.CreatePurchaseController(req, res))
-router.get("/getios", (req, res) => {
-  getSavedIosroute.getAllDbIosRecords(req, res);
-});
-router.post("/orderGiftcard", JToken, (req, res) => {
-  orderGiftCards.OrderGiftcardsProducts(req, res);
-});
-
-router.get("/getGiftCardProducts", JToken, (req, res) => {
-  getallgiftcardProducts.GetAllGiftCardProducts(req, res)
-})
-
-
-
-//Paystack fundings
-router.post("/create/dedicated/account/paystack", (req, res) => {
-  creatededicatedAccount.CreateDedicatedAccount(req, res)
-})
-
-
-//Storage for Firbase Cloud Messaging
-
-router.post("/store/token", (req, res) => {
-  fcmStorageController.fcmstorer(req, res)
-})
-
-
-
-//Get Single Purchased Data
+//############ Get Single Purchased Data ###############
 router.get("/purchase/data/:type/:ntransactionId/:userId", (req, res)=> {
   singleDataController.provideSingleData(req, res)
 })
-
 router.get('/purchase/list/:userId', (req, res) => {
   translistcontroller.getlist(req, res)
 })
 
+//############# OTP ####################
+router.post("/otp", (req, res) => otpcontroller.CreateHISTORYController(req, res))
+router.post('/otp/request', (req, res) => otpcontroller.recieveOTP(req, res))
+
+//########## Storage for Firbase Cloud Messaging ########
+router.post("/store/token", (req, res) => {
+  fcmStorageController.fcmstorer(req, res)
+})
+
+//##########
 export { router };
